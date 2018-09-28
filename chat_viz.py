c3bf11949e87294e34ee1295fb06f31b47684d67
XshreyasguptaX <shreyas.charming@gmail.com>
2018-09-29 01:01:16 +0530

    optimisations

--- chat_viz.py
+++ chat_viz.py
@@ -5,7 +5,7 @@ from dateutil.parser import *
 from datetime import *
 
 # open text file
-chat = open("test_data.txt", "r+")
+chat = open("/Users/glados/Documents/glaDOS\ Archives/test_data.txt", "r+")
 
 #function to check if string is date
 def is_date(string):
@@ -23,17 +23,9 @@ for lines in chat.readlines():
         chat_tstamp.append(lines)
 # print (chat_tstamp)
 
-#create a list with no timestamps
-chat_no_tstamp = []
+sender_dict_tstamp = {}
 for line in chat_tstamp:
-    _del_index = line.find('-')
-    chat_no_tstamp.append(line[_del_index+2:])
-# print(chat_no_tstamp)
-
-#create a dictionary with the sender name as dict keys
-sender_dict = {}
-for line in chat_no_tstamp:
-    colon_index = line.find(':')
-    name = line[:colon_index]
-    sender_dict[name] = line[colon_index:]
-# print(sender_dict.keys())
+    time_index = line.find("-")
+    colon_index = line[time_index+2:].find(':')
+    name_tstamp = line[time_index+2:colon_index+time_index+2]
+    sender_dict_tstamp[name_tstamp] = line
