f73cff01e2cff76b1fa8acc0489214c75f568e00
XshreyasguptaX <shreyas.charming@gmail.com>
2018-09-29 11:02:35 +0530

    removed redundant code

--- chat_viz.py
+++ chat_viz.py
@@ -6,23 +6,18 @@ chat = importer.chat_import("/Users/glados/Documents/test_data.txt")
 
 #segregate raw .txt file into individual texts
 def chat_parser(chat):
-    chat_tstamp = []
+    sender_dict_tstamp = {}
     for lines in chat.readlines():
-        comm_index = lines.find("-")
-        if(helperfunctions.is_date(lines[:comm_index])):
-            chat_tstamp.append(lines)
-    return chat_tstamp 
+        time_index = lines.find("-")
+        if(helperfunctions.is_date(lines[:time_index])):
+            colon_index = lines[time_index+2:].find(":")
+            name_tstamp = lines[time_index+2:colon_index+time_index+2]
+            if(name_tstamp in sender_dict_tstamp.keys()):
+                sender_dict_tstamp[name_tstamp].append(lines)
+            else:
+                sender_dict_tstamp[name_tstamp] = list(lines)
+    return sender_dict_tstamp 
 
 chat_tstamp = chat_parser(chat)       
-# print (chat_tstamp) 
-def sender_dict_create(parsed_chat): #create nested dicts
-    sender_dict_tstamp = {}
-    for line in parsed_chat:
-        time_index = line.find("-")
-        #helperfunctions.parse_datetime(line[:time_index])
-        colon_index = line[time_index+2:].find(':')
-        name_tstamp = line[time_index+2:colon_index+time_index+2]
-        sender_dict_tstamp[name_tstamp] = line
-        return sender_dict_tstamp
-#print(sender_dict_tstamp.keys())
-sender_dict_tstamp = sender_dict_create(chat_tstamp)
+#print (chat_tstamp["shreyas Gupta"]) 
+
