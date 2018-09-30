476d46831cf3465f21ca164a37baebc9e98e1d8a
XshreyasguptaX <shreyas.charming@gmail.com>
2018-09-30 08:36:07 +0530

    implemented JSON-like data structure

--- data_parser.py
+++ data_parser.py
@@ -1,7 +1,6 @@
 import helperfunctions
-from collections import defaultdict
 
-#segregate raw .txt file into a nested dict structure
+#segregate raw .txt file into a nested dict structure with JSON-like referencing
 def parse_chat(chat):
     sender_dict_tstamp = {}
     for lines in chat.readlines():
@@ -10,7 +9,13 @@ def parse_chat(chat):
             colon_index = lines[time_index+2:].find(":")
             name_tstamp = lines[time_index+2:colon_index+time_index+2]   
             if(name_tstamp in sender_dict_tstamp.keys()):
-                sender_dict_tstamp[name_tstamp].append(lines)
+                if (lines[:time_index] in sender_dict_tstamp[name_tstamp].keys()):
+                    sender_dict_tstamp[name_tstamp][lines[:time_index]].append(lines[colon_index+time_index+4:])
+                else:
+                    sender_dict_tstamp[name_tstamp][lines[:time_index]] = list()
+                    sender_dict_tstamp[name_tstamp][lines[:time_index]].append(lines[colon_index+time_index+4:])
             else:
-                sender_dict_tstamp[name_tstamp] = list(lines)
+                sender_dict_tstamp[name_tstamp] = dict()
+                sender_dict_tstamp[name_tstamp][lines[:time_index]] = list()
+                sender_dict_tstamp[name_tstamp][lines[:time_index]].append(lines[colon_index+time_index+4:])
     return sender_dict_tstamp 
