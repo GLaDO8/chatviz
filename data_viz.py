764d6cea0ed3dda3247fbb4fea76050c482ac860
XshreyasguptaX <shreyas.charming@gmail.com>
2018-09-30 08:37:46 +0530

    implemented JSON-like data structure

--- data_viz.py
+++ data_viz.py
@@ -1,10 +1,17 @@
 import importer
 import data_parser
 import matplotlib.pyplot as plt
+import numpy as np
 
 # open text file
 chat = importer.chat_import("/Users/glados/Documents/test_data.txt")
 
 chat_tstamp = data_parser.parse_chat(chat)
-
+# items = tuple(chat_tstamp["Sushma"].keys())
+# print (items)
 def plt_tseries_len(chat_tstamp):
+    for key in chat_tstamp.keys():
+        plt.plot(tuple(chat_tstamp[key].keys()), tuple(chat_tstamp[key].values()))
+    plt.legend()
+    plt.show()
+plt_tseries_len(chat_tstamp)
