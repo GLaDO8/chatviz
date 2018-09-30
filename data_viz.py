cc1eecddca8bbe544d3521c521601e3216893719
XshreyasguptaX <shreyas.charming@gmail.com>
2018-09-30 19:57:17 +0530

    fixed logical issues

--- data_viz.py
+++ data_viz.py
@@ -1,17 +1,24 @@
 import importer
 import data_parser
 import matplotlib.pyplot as plt
-import numpy as np
-
 # open text file
-chat = importer.chat_import("/Users/glados/Documents/test_data.txt")
 
-chat_tstamp = data_parser.parse_chat(chat)
-# items = tuple(chat_tstamp["Sushma"].keys())
-# print (items)
 def plt_tseries_len(chat_tstamp):
     for key in chat_tstamp.keys():
-        plt.plot(tuple(chat_tstamp[key].keys()), tuple(chat_tstamp[key].values()))
-    plt.legend()
-    plt.show()
-plt_tseries_len(chat_tstamp)
+        for tstamp in chat_tstamp[key].keys():
+            for texts in chat_tstamp[key][tstamp]:
+                text_len = 0
+                for text in texts:
+                    text_len = text_len + len(text)
+            chat_tstamp[key][tstamp].insert(0, text_len)
+    
+    for key in chat_tstamp.keys():
+        temp = []
+        for texts in chat_tstamp[key].values():
+            temp.append(texts[0])
+        # plt.plot(tuple(chat_tstamp[key].keys()), tuple(temp))
+        # plt.show()
+if __name__ == "__main__":
+    chat = importer.chat_import("/Users/glados/Documents/test_data.txt")
+    chat_tstamp = data_parser.parse_chat(chat) 
+    plt_tseries_len(chat_tstamp)
