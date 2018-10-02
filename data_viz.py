aa9666ff032037767a20e5f4d173072e8d8cf7c2
XshreyasguptaX <shreyas.charming@gmail.com>
2018-10-02 08:38:12 +0530

    can now generate plots

--- data_viz.py
+++ data_viz.py
@@ -11,13 +11,25 @@ def plt_tseries_len(chat_tstamp):
                 for text in texts:
                     text_len = text_len + len(text)
             chat_tstamp[key][tstamp].insert(0, text_len)
-    
-    for key in chat_tstamp.keys():
+    colors = ['r', 'g', 'b']
+    for i, key in enumerate(chat_tstamp.keys(), 0):
         temp = []
         for texts in chat_tstamp[key].values():
             temp.append(texts[0])
-        # plt.plot(tuple(chat_tstamp[key].keys()), tuple(temp))
-        # plt.show()
+        plt.plot(chat_tstamp[key].keys(), temp, colors[i])
+    plt.show()
+        # count = 0
+        # avg = [], mean_list1 = [], mean_list2 = []
+        # for i, val in enumerate(chat_tstamp[key].keys(), 1):
+        #     count = count+1
+        #     if (count <=7 && i!=len(chat_tstamp[key].keys())):
+        #         avg.append(val)
+        #     else if(count>7):
+        #         mean_list2.append(mean(avg))
+
+
+
+
 if __name__ == "__main__":
     chat = importer.chat_import("/Users/glados/Documents/test_data.txt")
     chat_tstamp = data_parser.parse_chat(chat) 
